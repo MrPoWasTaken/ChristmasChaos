1ad3295b1c1e5e0da2997b9c6345d8fcd6923f82
mathkimchi <kingalvinjkim@gmail.com>
2024-12-22 11:23:24 -0500

    basic 2d movement

---
+++ player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+
+@export 
+var SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	
+	self.velocity = SPEED * direction
+	
+	move_and_slide()
