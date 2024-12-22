ab615e7becb76b7f5576b394aed33c408b99f4c4
mathkimchi <kingalvinjkim@gmail.com>
2024-12-22 17:06:49 -0500

    shoot bullet on click

--- player.gd
+++ player.gd
@@ -13,6 +13,7 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _input(event):
+	# below code is disgusting btw
 	if Input.is_action_pressed("ui_left"):
 		$Icon/AnimationPlayer.play("walk_left")
 	elif Input.is_action_pressed("ui_right"):
@@ -30,3 +31,11 @@ func _input(event):
 		$Icon/AnimationPlayer.play("idle_up")
 	elif Input.is_action_just_released("ui_down"):
 		$Icon/AnimationPlayer.play("idle_down")
+		
+	if event is InputEventMouseButton and event.is_pressed():
+		const BULLET = preload("res://bullet.tscn")
+		var new_bullet = BULLET.instantiate()
+		new_bullet.global_position = self.global_position
+		new_bullet.global_rotation = self.get_global_transform_with_canvas().get_origin().direction_to(event.position).angle()
+		
+		self.add_sibling(new_bullet)
