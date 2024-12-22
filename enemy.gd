9f6cbb08346c5d9ba9cfac49071924b7337c2116
mathkimchi <kingalvinjkim@gmail.com>
2024-12-22 17:56:17 -0500

    make enemies progressively faster

--- enemy.gd
+++ enemy.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+
+## NOT ACTUALLY A CONSTANT, LOOK AT _ready
 @export
 var SPEED = 150.0
 
@@ -13,6 +15,10 @@ var player: CharacterBody2D = get_node("/root/MainLevel/Player")
 
 func _ready() -> void:
 	add_to_group("Enemies")
+	
+	# enemies spawn slightly faster as there are more of them
+	SPEED = SPEED + 5. * get_tree().get_node_count_in_group("Enemies")
+	print("SPEED:", SPEED)
 
 func _physics_process(delta: float) -> void:
 	var player_direction = global_position.direction_to(player.global_position)
