d87aa1a414e296d224a6dc4e95ff2fb5f1c73db8
MrPoWasTaken <jerrygw2004@gmail.com>
2024-12-22 12:54:53 -0500

    test animation for player

--- player.gd
+++ player.gd
@@ -11,3 +11,16 @@ func _physics_process(delta: float) -> void:
 	self.velocity = SPEED * direction
 	
 	move_and_slide()
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_left"):
+		$Icon/AnimationPlayer.play("walk_left")
+	elif Input.is_action_just_pressed("ui_right"):
+		$Icon/AnimationPlayer.play("walk_right")
+	elif Input.is_action_just_pressed("ui_up"):
+		$Icon/AnimationPlayer.play("walk_up")
+	elif Input.is_action_just_pressed("ui_down"):
+		$Icon/AnimationPlayer.play("walk_down")
+		
+	if Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_down") or Input.is_action_just_released("ui_up") or Input.is_action_just_released("ui_right"):
+		#add idle animation here
