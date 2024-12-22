bf79963fac3c8fb0bd5804b3fa4af7be4d1c4002
mathkimchi <kingalvinjkim@gmail.com>
2024-12-22 12:20:20 -0500

    bullet collide with enemy

--- bullet.gd
+++ bullet.gd
@@ -1,8 +1,24 @@
 extends Area2D
 
+
 @export
 var SPEED = 300
+
+@export
+var RANGE = 2000
+
+var traveled_distance = 0
 
 func _physics_process(delta: float) -> void:
 	var direction = Vector2.RIGHT.rotated(self.global_rotation)
 	position += delta * SPEED * direction
+	
+	traveled_distance += delta * SPEED
+	if traveled_distance > RANGE:
+		queue_free() # self destruct
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Enemies"):
+		body.take_damage()
+		self.queue_free() # self destruct
--- enemy.gd
+++ enemy.gd
@@ -6,9 +6,16 @@ var SPEED = 150.0
 @onready
 var player: CharacterBody2D = get_node("/root/MainLevel/Player")
 
+func _ready() -> void:
+	add_to_group("Enemies")
+
 func _physics_process(delta: float) -> void:
 	var player_direction = global_position.direction_to(player.global_position)
 	
 	self.velocity = SPEED * player_direction
 	
 	move_and_slide()
+	
+	
+func take_damage() -> void:
+	print("Ouchies!")
